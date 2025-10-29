ea79541f251383d4fdf93bc7d9c55bb033c7106b
tonmoyd0116 <tonmoydey598@gmail.com>
2025-10-29 19:49:14 +0530

    Updated push for Customer Registration without RBAC

--- src/seller/seller.controller.ts
+++
@@ -1,9 +0,0 @@
-import {Get,Controller} from '@nestjs/common'
-
-@Controller('seller')
-export class SellerController{
-    @Get('health-check')
-    healthCheck(){
-        return 'This is a health check for Seller';
-    }
-}
