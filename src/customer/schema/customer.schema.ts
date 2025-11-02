2cc302d0de2d053851e723ce29bb12acd6f601dc
tonmoyd0116 <tonmoydey598@gmail.com>
2025-11-02 19:55:20 +0530

    Adding Argo2id implementation for hashing all passwords; Validation using DTO and enabling Global validation in main.ts

--- src/customer/schema/customer.schema.ts
+++ src/customer/schema/customer.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document, Model } from "mongoose";
+import * as argon2 from "argon2";
 
 @Schema({ timestamps: true })
 export class Customer extends Document {
@@ -15,12 +16,36 @@ export class Customer extends Document {
 
 export const CustomerSchema = SchemaFactory.createForClass(Customer);
 
-// ✅ Custom static method
+//  Adding argon2 for hashing purpose : Note that encryption is not same as hashing; the former one is reversible and the latter isn't
+CustomerSchema.pre("save",async function(next){
+  const customer = this as Customer;
+
+  // if the password was modified then don't do anything and return the next
+  if(!customer.isModified("password")) return next();
+
+  try{
+    const hash = await argon2.hash(customer.password,{
+      type:argon2.argon2id,
+      timeCost: 3,
+      memoryCost:2**16,
+      parallelism: 1,
+    }); 
+
+    // replace the password with the hash 
+    customer.password = hash;
+
+    // do the next steps
+    next();
+  }catch(err){
+    next(err);
+  }
+});
+
+
 CustomerSchema.statics.findByEmail = function (email: string) {
   return this.findOne({ email });
 };
 
-// ✅ Custom model interface
 export interface CustomerModel extends Model<Customer> {
   findByEmail(email: string): Promise<Customer | null>;
 }
